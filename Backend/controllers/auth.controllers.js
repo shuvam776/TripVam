82cce5cc9efc9ef058e228e6f678916a455c6e74
shuvam_776 <shuvamsatapathi@gmail.com>
2025-12-29 00:31:05 +0530

    search controller

--- Backend/controllers/auth.controllers.js
+++ Backend/controllers/auth.controllers.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken"
-import User from "../models/user.model.js"
+import User from "../models/User.model.js"
 import { verifyGoogleToken } from "../services/google.service.js"
 
 const createToken = (userId) =>
